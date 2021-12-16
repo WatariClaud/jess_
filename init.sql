791b7f692e9d1f47b1b9da96a3cdaa3f9b4d7949
ClaudWatari <claudwatari95@gmail.com>
2021-12-16 16:53:38 +0300

    Update init.sql

--- init.sql
+++ init.sql
@@ -2,4 +2,4 @@ CREATE TABLE users (id SERIAL NOT NULL , name VARCHAR(50) NOT NULL , email VARCH
 
 CREATE TABLE parking (id SERIAL NOT NULL , space VARCHAR(100) NOT NULL , spots INT NOT NULL , priceperspot VARCHAR(50) NOT NULL);
 
-CREATE TABLE tickets (id SERIAL PRIMARY KEY, userid integer, datecreated VARCHAR (50), status VARCHAR (11), ispaid boolean, amount VARCHAR (50), spaceid VARCHAR(100));
+CREATE TABLE tickets (id SERIAL PRIMARY KEY, userid integer, datecreated VARCHAR (50), status VARCHAR (11), ispaid boolean, amount VARCHAR (50), spaceid VARCHAR(100), datepaid VARCHAR (50));
