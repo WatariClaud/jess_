a864b5496a7c9e6eef6e59ec7aa5fcf76bba5cfd
ClaudWatari <claudwatari95@gmail.com>
2023-10-05 17:30:32 +0300

    Update Database.php

--- config/Database.php
+++ config/Database.php
@@ -5,17 +5,17 @@
         // private $dbname = 'jess';
         // private $username = 'root';
         // private $password  = '';
-        // private $connection;
+        private $connection;
         
-        // private $host = 'us-cdbr-east-04.cleardb.com';
-        // private $dbname = 'heroku_2b643ff81dafd74';
-        // private $username = 'b9eab9715fd32d';
-        // private $password  = 'cbfe69e7';
+        private $host = 'us-cdbr-east-04.cleardb.com';
+        private $dbname = 'heroku_2b643ff81dafd74';
+        private $username = 'b9eab9715fd32d';
+        private $password  = 'cbfe69e7';
         
-        private $host = 'viaduct.proxy.rlwy.net';
-        private $dbname = 'railway';
-        private $username = 'root';
-        private $password  = 'ajv478ngba6b7si6gcbeosl5xtlv2qc$';
+        // private $host = 'viaduct.proxy.rlwy.net';
+        // private $dbname = 'railway';
+        // private $username = 'root';
+        // private $password  = 'ajv478ngba6b7si6gcbeosl5xtlv2qc$';
 
         public function connect() {
             $this->connection = null;
